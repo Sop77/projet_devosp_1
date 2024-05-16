7bdabe4d3392d81d1a3c74f696782cd509ecb436
Sop77 <125497749+Sop77@users.noreply.github.com>
2024-05-16 17:13:22 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,8 +11,9 @@ terraform {
 
 # Déclaration du provider Kubernetes
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  config_path = "C:/Users/HP/.kube/config"
 }
+
 
 # Récupération du contenu YAML pour le déploiement PHP
 resource  "kubernetes_manifest" "php_deployment" {
